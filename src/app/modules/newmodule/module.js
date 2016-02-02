d01be20b785e9a0c2f8fed4c831c9b4566d0abe2
openedbox <openedbox@qq.com>
2016-02-02 14:59:27 +0800

    xinyu #na bind controller and view for newmodule

--- src/app/modules/newmodule/module.js
+++ src/app/modules/newmodule/module.js
@@ -10,7 +10,9 @@
     $stateProvider
       .state('newmodule', {   //更改demo为newmodule
         url: '/newmodule',      //更改demo为newmodule 这个url将
-        templateUrl: 'app/modules/newmodule/index.html' //指向我们的view
+        templateUrl: 'app/modules/newmodule/index.html', //指向我们的view
+        controller:'newModuleCtrl', //控制器的名称
+        controllerAs:'viewModel'    //别名
       });
   }
 })();
