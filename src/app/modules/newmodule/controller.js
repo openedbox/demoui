42992a01a52948081a5be95ab418c74f99718951
openedbox <openedbox@qq.com>
2016-02-02 14:48:18 +0800

    xinyu #na change controller.js for newmodule

--- src/app/modules/newmodule/controller.js
+++ src/app/modules/newmodule/controller.js
@@ -3,10 +3,11 @@
 
   angular
     .module('demoui')
-    .controller('demoCtrl', demoCtrl);
+    .controller('newModuleCtrl', newModuleCtrl); //对应改名
 
   /** @ngInject */
-  function demoCtrl() {
-
+  function newModuleCtrl() {
+     var viewModel = this;    //定义视图模型 用于承载视图上需要显示的数据
+     viewModel.customVariable = "this is custom variable blablabla";  //在视图模型增加一个变量
   }      
 })();
